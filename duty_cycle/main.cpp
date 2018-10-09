7e5a9dfd76580fdf19a5285dc5dbeeb77c863bf9
Richard Reich (Real-Time Market Data) <rreich@ny-lxdesk214.mlp.com>
2018-10-09 14:53:43 -0400

    Intermediate commit

--- duty_cycle/main.cpp
+++ duty_cycle/main.cpp
@@ -4,15 +4,55 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 
+constexpr uint32_t g_precision = 10000;
+
+struct Results
+{
+    Results() {}
+    uint16_t saturationCycles_{0};
+    uint16_t saturationRatio_{0};
+    uint16_t bandwidth_{0};
+    uint8_t controlBits_{0};
+};
+
+union ResultsSync
+{
+    alignas(sizeof(uint64_t)) Results results_;
+    std::atomic<uint64_t> atomicCopy_;
+
+    ResultsSync() : atomicCopy_(0) {}
+
+    ResultsSync& operator=(ResultsSync& rs)
+    {
+        if (&rs == this)
+            return *this;
+
+        //std::atomic<uint64_t>& ac = *reinterpret_cast<std::atomic<uint64_t>*>(&atomicCopy_);
+        //std::atomic<uint64_t>& rs_ac = *reinterpret_cast<std::atomic<uint64_t>*>(&rs.atomicCopy_);
+        
+        std::atomic<uint64_t>& ac = atomicCopy_;
+        std::atomic<uint64_t>& rs_ac = rs.atomicCopy_;
+
+        ac.store(rs_ac.load(std::memory_order_acquire), std::memory_order_release);
+
+        return *this;
+    }
+};
+
 struct CycleTracker
 {
-    std::atomic<uint64_t> start_{0};
-    std::atomic<uint64_t> end_{0};
-    std::atomic<uint64_t> overhead_{0};
-    std::atomic<uint64_t> saturation_{0};
-    std::atomic<uint32_t> polls_{0};
-    std::atomic<uint32_t> works_{0};
-    std::atomic<uint32_t> controlFlags_{0};
+    ResultsSync rs_;
+    Results& results_{rs_.results_};
+
+    uint64_t start_{0};
+    uint64_t end_{0};
+    uint64_t overhead_{0};
+    uint64_t saturation_{0};
+    uint32_t polls_{0};
+    uint32_t works_{0};
+    uint32_t controlFlags_{0};
+
+    CycleTracker() : results_(rs_.results_) {}
 
     enum ControlFlags : uint32_t
     {
@@ -22,58 +62,80 @@ struct CycleTracker
 
     void start()
     {
-        start_.store(getcc_b());
+        start_ = getcc_b();
     }
 
     void end()
     {
-        end_.store(getcc_e());
+        end_ = getcc_e();
+    }
+
+    void calcResults()
+    {
+        results_.saturationCycles_ = saturationCycles();
+        results_.saturationRatio_ = saturationRatio();
+        results_.bandwidth_ = (end_ - start_) * works_;
+    }
+
+    Results getResults()
+    {
+        ResultsSync r;
+        r = rs_;
+        return r.results_;
     }
 
     uint64_t bandwidth()
     {
+        // busy wait on clear flag
         return (end_ - start_) * works_;
     }
 
-    float saturationCycles()
+    uint16_t saturationCycles()
     {
         if (saturation_)
-            return static_cast<float>(saturation_) / (saturation_ + overhead_);
+            return static_cast<uint16_t>(   (static_cast<float>(saturation_) / (saturation_ + overhead_))* g_precision    );
         else
             return 0;
     }
 
-    float saturationRatio()
+    uint16_t saturationRatio()
     {
         if (polls_)
-            return static_cast<float>(works_) / polls_;
+            return static_cast<uint16_t>((works_*g_precision) / polls_);
         else
             return 0;
     }
 
     void clear()
     {
-        if (controlFlags_.load(std::memory_order_acquire) & ControlFlags::Clear)
+        if (controlFlags_ & ControlFlags::Clear)
         {
             std::cout << "Clearing" << std::endl;
-            overhead_.store     (0, std::memory_order_relaxed);
-            saturation_.store   (0, std::memory_order_relaxed);
-            polls_.store        (0, std::memory_order_relaxed);
-            works_.store        (0, std::memory_order_relaxed);
-            controlFlags_.fetch_and(~ControlFlags::Clear, std::memory_order_release);
+            overhead_       = 0;//(0, std::memory_order_relaxed);
+            saturation_     = 0;//(0, std::memory_order_relaxed);
+            polls_          = 0;//(0, std::memory_order_relaxed);
+            works_          = 0;//(0, std::memory_order_relaxed);
+            controlFlags_   &= ~ControlFlags::Clear;
+            //controlFlags_.fetch_and(~ControlFlags::Clear, std::memory_order_release);
         }
     }
 
     void addOverhead (uint64_t o)
     {
-        overhead_.fetch_add(o, std::memory_order_acquire);
-        polls_.fetch_add(1, std::memory_order_release);
+        overhead_ += o;
+        ++polls_;
+
+        //overhead_.fetch_add(o, std::memory_order_acquire);
+        //polls_.fetch_add(1, std::memory_order_release);
     }
 
     void addDuty(uint64_t d)
     {
-        saturation_.fetch_add(d, std::memory_order_acquire);
-        works_.fetch_add(1, std::memory_order_release);
+        saturation_ += d;
+        ++works_;
+
+        //saturation_.fetch_add(d, std::memory_order_acquire);
+        //works_.fetch_add(1, std::memory_order_release);
     }
 
     struct CheckPoint
@@ -87,13 +149,11 @@ struct CycleTracker
             ct_.clear();
 
             if (p2_)
-            {
                 ct_.addOverhead(p2_ - p1_);
-            }
             if (p3_)
-            {
                 ct_.addDuty(p3_ - p2_);
-            }
+
+            ct_.calcResults();
         }
 
         void markOne() { p1_ = getcc_ns(); }
@@ -126,18 +186,34 @@ int main ( int argc, char* argv[] )
         }
         cp.markTwo();
 
-        for(int k = 0; k < 200; ++k)
+        for(int k = 0; k < 5; ++k)
             getcc_ns();
         cp.markThree();
     }
     ct.end();
+    ct.calcResults();
+
+    Results r;
+    r = ct.getResults();
 
     std::cout << "Sizeof CycleTracker = " << sizeof(CycleTracker) << std::endl;
+    
     std::cout << "Overhead = " << ct.overhead_ << std::endl;
     std::cout << "Duty = " << ct.saturation_ << std::endl;
 
-    std::cout << "work saturation [Cycles] = " << ct.saturationCycles() << std::endl;
-    std::cout << "ratio saturation [Ratio] = " << ct.saturationRatio() << std::endl;
-    std::cout << "Bandwidth [work/ms] = " << ct.bandwidth() / (3.0 * 1'000'000) << std::endl;
+    std::cout << "works_ = " << ct.works_ << std::endl;
+    std::cout << "polls_ = " << ct.polls_ << std::endl;
+
+    std::cout << "start_ = " << ct.start_ << std::endl;
+    std::cout << "end_ = " << ct.end_ << std::endl;
+    std::cout << "end_ - start_ = " << ct.end_ - ct.start_ << std::endl;
+
+    // need to make fetcher methods to hide the g_precision and ugly static_cast
+    std::cout << "saturation [Cycles] = " << static_cast<float>(r.saturationCycles_)/g_precision << std::endl;
+    std::cout << "saturation [Ratio] = " << static_cast<float>(r.saturationRatio_)/g_precision << std::endl;
+    std::cout << "Bandwidth [work/ms] = " << static_cast<float>(r.bandwidth_) / (3.0 * 1'000'000) << std::endl;
+
+    // after observation 
+    ct.clear();
 	return 0;
 }
